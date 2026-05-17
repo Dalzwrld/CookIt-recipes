91c3d1aab57860c8924217182510cbf326f59a72
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-05-18 00:53:00 +0300

    feat: add recipe form && recipe list

---
+++ src/components/app/recipe-form.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function RecipeForm() {
+  return (
+    <div className="">
+        <div className="">
+            <h2 className="">Add your Recipe/Update your Recipe</h2>
+            <h6 className="">Write your favorite recipe down below/Change the details of your recipe</h6>
+        </div>
+
+        
+    </div>
+  )
+}
---
+++ src/components/app/recipe-list.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function RecipeList() {
+  return (
+    <div className="">
+        <div className="">
+            <h2 className="">Recipes</h2>
+            <h6 className="">2 Recipes</h6>
+        </div>
+    </div>
+  )
+}
