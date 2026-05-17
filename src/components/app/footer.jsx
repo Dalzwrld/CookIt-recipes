c17490ce116047911e35e10b64f241607d4e8e96
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-05-18 00:33:00 +0300

    feat: add footer

--- src/components/app/footer.jsx
+++ src/components/app/footer.jsx
@@ -1,7 +1,18 @@
+import { Copyright } from 'lucide-react'
 import React from 'react'
 
 export default function Footer() {
   return (
-    <div></div>
+    <div>
+        <span className="uppercase">CookIt</span>
+        <span className="uppercase">
+            Images by
+            <a href="">Unsplash</a>
+        </span>
+        <span className="uppercase">
+            <Copyright className="size-2" />
+            2026. Built with React.
+        </span>
+    </div>
   )
 }
